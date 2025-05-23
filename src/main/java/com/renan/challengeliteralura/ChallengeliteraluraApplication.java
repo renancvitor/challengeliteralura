f69c01423bdd099221540c1c5d9169b10ec31dd4
renancvitor <renan.vitor.cm@gmail.com>
2025-05-23 19:02:46 -0300

    Inicialização do Challenge

---
+++ src/main/java/com/renan/challengeliteralura/ChallengeliteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.renan.challengeliteralura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeliteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeliteraluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renan/challengeliteralura/ChallengeliteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.challengeliteralura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeliteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
